8bc85b99c4c2d68f300736026f1908f57f1984bd
Christophe Gagnier <Moustachauve@users.noreply.github.com>
2022-02-08 01:39:31 -0500

    Initial commit

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name='pytechnove',
+    version='0.1.0',
+    description='TechnoVE local device API library',
+    url='https://github.com/moustachauve/pytechnove',
+    author='Christophe Gagnier',
+    license='GNU General Public License v3 (GPLv3)',
+    packages=['pytechnove'],
+    install_requires=[],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.9',
+        'Topic :: Software Development :: Libraries',
+    ],
+)
